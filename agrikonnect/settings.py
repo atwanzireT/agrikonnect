3609dca1bdf71af29242aefcd953abf98074f657
atwanzireT <tatwanzire@gmail.com>
2026-06-02 11:13:32 +0300

    worked on db

--- agrikonnect/settings.py
+++ agrikonnect/settings.py
@@ -94,10 +94,22 @@ WSGI_APPLICATION = 'agrikonnect.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "agrikonnect",
+        "USER": "postgres",
+        "PASSWORD": "atwanzire",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
